7c854ddbaf88eb4fa1450620ec22e1a87c4abd54
CChoi08 <C_Choi7895@yahoo.com>
2022-11-28 10:29:08 -0800

    first commit

---
+++ JavaFundamentals/FirstJavaProgram/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static void main(String[] args) {
+        String name = "Chris";
+        int age = 27;
+        String hometown = "Oakland, CA";
+
+        System.out.println("My name is " + name);
+        System.out.println("I am " + age + " years old");
+        System.out.println("My hometown is " + hometown);
+    }
+}
